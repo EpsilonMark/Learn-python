c700f7207514be5807b98e2e741dbb1b96f7db03
Jathurawit <Jathurawit_mark@hotmail.com>
2021-07-03 14:01:25 +0700

    Edit 2

--- tuple_demo.py
+++ tuple_demo.py
@@ -1,3 +1,19 @@
-print('hello world')
-print('tea')
-print('Mark')
+#print('hello world')
+#print('tea')
+#print('Mark')
+
+#point = 1,7
+#print(point[0])
+#print(point[1])
+
+#th = 'ThaiLand',5 ,10, 12
+#print(th[1]+th[2]+th[3])
+
+def distance(pointA , pointB):
+    return ((pointA[0]- pointB[0])**2 +(pointA[1]-pointB[1])**2 ) **.5
+
+pointA = 1,7
+pointB = 1,10
+d = distance(pointA,pointB)
+
+print(d)
